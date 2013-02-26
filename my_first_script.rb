a1524e7bf24db32dc047482f852b8b14fa503a48
Amit Runchal <amitrunchal@gmail.com>
2013-02-25 21:14:51 -0800

    Updated todo, changed name

--- my_first_script.rb
+++ my_first_script.rb
@@ -1,5 +1,5 @@
-first_name = "Kris"
-last_name = "Smith"
+first_name = "Amit"
+last_name = "Runchal"
 age ="34"
 
 puts first_name + " " + last_name + " is " + age
--- todo.md
+++ todo.md
@@ -1,1 +1,4 @@
 test
+hello people
+what the fuck is going on here
+i live for this shit, motherfucker
